bd24f782f11d820c946ce93d7b81c8d989576fe9
Saddam Anwar <saddamanwarllc@gmail.com>
2025-07-17 16:37:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true })); // Parses URL-encoded bodies
 app.use(bodyParser.json()); // Parses JSON (redundant with express.json but ok if needed)
 app.use(cookieParser()); // Parses cookies from the request headers
 app.use(cors({
-  origin: 'https://saddamAnwar185.github.io',
+  origin: 'https://cafae-saddam-anwars-projects.vercel.app',
   credentials: true,
   methods: ['POST', 'GET', 'DELETE', 'PUT', 'PATCH']
 }));
