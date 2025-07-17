5a9f4609a6a050c0659ae53b3eca31665b400599
Saddam Anwar <saddamanwarllc@gmail.com>
2025-07-17 16:46:36 +0500

    Update Model.js

--- Model/Model.js
+++ Model/Model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 
-const url = process.env.MONGODB_URl
+const url = process.env.MONGODB_URL
 
 mongoose.connect(url).then(()=>{
     console.log('connected')
