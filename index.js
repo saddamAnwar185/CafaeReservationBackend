2b00d3fdc2f04ddf734bc8fa15279aa1621a8da8
Saddam Anwar <saddamanwarllc@gmail.com>
2025-07-17 16:49:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({ override: true })
 const app = express()
 const PORT = process.env.PORT
 
